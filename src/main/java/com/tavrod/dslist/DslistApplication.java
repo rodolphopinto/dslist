22ce7e7e574264e398c0e4a94e5b2d05ba168316
Rodolpho Tavares Pinto <rodolphopinto@hotmail.com>
2025-01-28 21:50:14 -0300

    Projeto criado

---
+++ src/main/java/com/tavrod/dslist/DslistApplication.java
@@ -0,0 +1,14 @@
+package com.tavrod.dslist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslistApplication {
+
+	public static void main(String[] args) {
+		System.out.println("By Rodolpho Tavares");
+		SpringApplication.run(DslistApplication.class, args);
+	}
+
+}
